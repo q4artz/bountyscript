2cf3ca44c289a72647d2a3146a4764c3114155d8
q4artz <116222352+q4artz@users.noreply.github.com>
2023-09-09 21:22:14 +0800

    Add files via upload

--- findsubdomains.sh
+++ findsubdomains.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#!/bin/python3
 
 #getting arguements
 url=$1
 
+#check if directory exist
 if [ ! -d "$url" ]; then 
 mkdir $url
 fi
@@ -11,28 +13,35 @@ if [ ! -d "$url/recon" ];then
 mkdir $url/recon
 fi
 
-function Harvesting(){
-	if [ $option == 1 ]; then
-		#running assetfinder
-		echo "[+] Harvesting Subdomains with assetfinder"
-		assetfinder $url >> $url/recon/assets.txt
-		cat $url/recon/assets.txt | grep $url >> $url/recon/assetfinderSubs.txt 
-		echo "[-] Subdomains Found by assetfinder added to" $url/recon/assetfinderSubs.txt
-	
-	 elif [ $option == 2 ]; then
-		#running Amass
-		echo "[+] Harvesting Subdomains with Amass"
-		amass enum -d $url >> $url/recon/f.txt
-		cat $url/recon/assets.txt | grep $url >> $url/recon/amassSubs.txt
-		echo "[+] Subdomains Found by Amass added to " $url/recon/amassSubs.txt
-	fi
-	rm $url/recon/assets.txt
-}
+if [ ! -d "$url/recon/aliveScreenshots" ];then
+mkdir $url/recon/aliveSreenshots
+fi
 
-$(Harvesting(1)) 
+#running assetfinder
+echo "[+] Harvesting Subdomains with assetfinder"
+assetfinder $url >> $url/recon/assets.txt
+cat $url/recon/assets.txt | grep $url >> $url/recon/assetfinderSubs.txt 
+echo "[+] assetfinder Found " $(cat $url/recon/assetfinderSubs.txt | wc -l) "Subdomains"
+echo "[+] Subdomains Found by assetfinder added to" $url/recon/assetfinderSubs.txt
+rm $url/recon/assets.txt
+echo -e "\n"
 
-#old code that could be reused
-#$(cat $url/recon/relevantSubdomains.txt | grep $url | wc -l >> $url/recon/relevantSubdomains.txt) 
-#(echo "Subdomains Found" >> $url/recon/relevantSubdomains.txt)
+#running Amass
+#echo "[+] Harvesting Subdomains with Amass"
+#amass enum -d $url >> $url/recon/assets.txt
+#sort $url/recon/assets.txt >> $url/recon/amassSubs.txt
+#echo "[+] Amass found " $(cat $url/recon/amassSubs.txt | wc -l) "Subdomains"
+#echo "[+] Subdomains Found by Amass added to " $url/recon/amassSubs.txt
+#rm $url/recon/assets.txt
+#echo -e "\n"
 
+#running httprobe
+echo "[+] Running Httprobe"
+cat $url/recon/assetfinderSubs.txt | sort -u | httprobe -s -p https:443| sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/aliveSubs.txt
+echo -e "[+] Httprobe found " $(cat $url/recon/aliveSubs.txt | wc -l) "Alive subdomains\n"
+
+#capturing screenshots with gowitness
+echo "[+] Running Eyewitness"
+eyewitness --web -f $url/recon/aliveSubs.txt -d $url/recon/aliveScreenshots --resolve
+echo "[+] Screenshots put into " $url/recon/aliveScreenshots 
 
