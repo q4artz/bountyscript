e3a792a34b4a3a22b3edcbaed964cc7c2c3436e6
q4artz <116222352+q4artz@users.noreply.github.com>
2023-09-25 14:36:06 +0800

    Add files via upload

--- credsfind.sh
+++ credsfind.sh
@@ -26,16 +26,23 @@ echo -e "[+] Running theHarvester..."
 sudo theHarvester -d $domain -b all -f $domain-creds/theHarvesterScan/
 echo -e "[+] Finished Running theHarvester..."
 
-#sort theHarvester emails into text file fro h8mail
+#sort theHarvester emails into text file for h8mail
 echo -e "[+] Extracting emails from theHarvester's file..."
 grep -oP '(?<=<email>).*?(?=</email>)' $domain-creds/theHarvesterScan/$(echo "$domain" | tr -d .com).xml > $domain-creds/theHarvesterScan/allemail.txt 
 echo -e "[+] Finished Extracting emails..."
 
-#Run h8mail
-echo -e "[+] Running h8mail..."
-sudo h8mail -t $domain-creds/theHarvesterScan/allemail.txt -c config.ini -o $domain-creds/h8mailScan/h8mailinfo.csv -j $domain-creds/h8mailScan/h8mailinfo.json
-echo -e "[+] FinishedRunning h8mail..."
-
 #Run breach-Parse
 echo -e "[+] Running breach-Parse..."
+sudo ./breach-parse/breach-parse.sh @$domain $domain-BP.txt "/opt/BreachCompilation/data"
 echo -e "[+] Finished Running breach-Parse..."
+
+#combine emails of breach-Parse and theHarvester
+mv tesla-master.txt tesla-passwords.txt tesla-users.txt tesla.com-creds/breachparseScan
+cat $domain-creds/theHarvesterScan/allemail.txt >> $domain-creds/Allemails.txt
+cat $domain-creds/breachparseScan/tesla-users.txt >> $domain-creds/Allemails.txt
+
+#Run h8mail
+echo -e "[+] Running h8mail..."
+sudo h8mail -t $domain-creds/Allemails.txt -c config.ini -o $domain-creds/h8mailScan/h8mailinfo.csv -j $domain-creds/h8mailScan/h8mailinfo.json
+echo -e "[+] FinishedRunning h8mail..."
+
